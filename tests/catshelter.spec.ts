4ef213d0cd6094315803a2afcc420a5d0f63c15d
WillHDev <will.v.holcombe@gmail.com>
2024-10-19 10:20:29 -0500

    edit test is working

--- tests/catshelter.spec.ts
+++ tests/catshelter.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-
 test.describe('Cat shelter', () => {
     test('Add new cat', async ({ page }) => {
         await page.goto('/catshelter');
@@ -12,15 +11,25 @@ test.describe('Cat shelter', () => {
         await page.getByRole('button' , { name:"Add Cat" }).click();
         const mrWhiskers = page.getByText('Mr. Whiskers');
         await expect(mrWhiskers).toBeVisible();
-        //
+      });
+
+    test('Edit cat', async ({ page }) => {
+        await page.goto('/catshelter');
+        const mrWhiskers = page.getByText('Mr. Whiskers');
+        await mrWhiskers.click();
+        await page.locator('input[name="name"]').fill('Joey Donuts');
+        await page.getByRole('button', { name: "Save" }).click();
+        const joeyDonuts = page.getByText('Joey Donuts');
+        await expect(joeyDonuts).toBeVisible();
       });
   
     test('Remove cat from shelter', async ({ page }) => {
-      await page.goto('/catshelter');
-      const mrWhiskers = page.getByText('Mr. Whiskers');
-      await expect(mrWhiskers).toBeVisible();
-      await mrWhiskers.click();
-      await page.getByRole('button', { name: "Delete" }).click();
-      await expect(mrWhiskers).not.toBeVisible();
+        await page.goto('/catshelter');
+        const joeyDonuts = page.getByText('Joey Donuts');
+        const mrWhiskers = page.getByText('Mr. Whiskers');
+        await joeyDonuts.click();
+        await page.getByRole('button', { name: "Delete" }).click();
+        await expect(mrWhiskers).not.toBeVisible();
+        await expect(joeyDonuts).not.toBeVisible();
     });
   });
